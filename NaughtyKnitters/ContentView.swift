93ef3e001de83e9f369282bfe43af1a5825e6d32
Steve Redden <no1pine@hotmail.com>
2026-08-16 15:33:07 -0700

    Initial Commit

---
+++ NaughtyKnitters/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NaughtyKnitters
+//
+//  Created by Steve Redden on 2026-08-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NaughtyKnitters/NaughtyKnittersApp.swift
@@ -0,0 +1,17 @@
+//
+//  NaughtyKnittersApp.swift
+//  NaughtyKnitters
+//
+//  Created by Steve Redden on 2026-08-16.
+//
+
+import SwiftUI
+
+@main
+struct NaughtyKnittersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
